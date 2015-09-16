07fe5306e610a3f9ee25494622ed6a3f30f2bade
David Shanabrook <davidshanabrook@me.com>
2015-09-15 20:31:37 -0400

    plottingFeatures
    series plotting and multiple lines.

--- valueAnalysis.R
+++ valueAnalysis.R
@@ -1,24 +1,34 @@
-library("zoo")
-library("ggplot2")
-library("PerformanceAnalytics")
-library("xts")
-valueFile <- "~/ShinyApps/tosAsync/out/value.csv"
-timeFormat <- "%Y-%m-%d %H:%M:%S"
-data <- read.csv(valueFile)
-data.zoo <- read.zoo(valueFile, header=T, sep=",")
-data.xts <- as.xts(data.zoo)
-data.zoo <- subset(data.zoo, select=-c(mom,dad))
-#data.zoo$all <- rowSums(data.zoo)
-data.zoo$mine <- data.zoo$roll + data.zoo$ira + data.zoo$reg
+	⁃	library("zoo")
+	⁃	library("ggplot2")
+	⁃	library("PerformanceAnalytics")
+	⁃	library("xts")
+	⁃	library(scales)
+	⁃	valueFile <- "~/ShinyApps/tosAsync/out/value.csv"
+	⁃	timeFormat <- "%Y-%m-%d %H:%M:%S"
+	⁃	data <- read.csv(valueFile)
+	⁃	data.zoo <- read.zoo(valueFile, header=T, sep=",")
+	⁃	data.xts <- as.xts(data.zoo)
+	⁃	data.zoo <- subset(data.zoo, select=-c(mom,dad))
+	⁃	data.zoo$all <- rowSums(data.zoo)
+	⁃	#data.zoo$mine <- data.zoo$roll + data.zoo$ira + data.zoo$reg
+	⁃	
+	⁃	#http://www.inside-r.org/packages/cran/zoo/docs/autoplot.zoo
+	⁃	zoo.df = fortify(data.zoo, melt = TRUE)
+	⁃	
+	⁃	p <- ggplot(aes(x = Index, y = Value, group=Series,colour=Series), data = zoo.df) + geom_line() + xlab("Index") +  scale_y_continuous("Value $", labels=comma) 
+	⁃	#+ scale_x_date()
+	⁃	  p + facet_grid(Series~., scale="free_y", labeller="label_value")  
+	⁃	  p  
 
-#http://www.inside-r.org/packages/cran/zoo/docs/autoplot.zoo
+# chart.CumReturns(data.zoo$, wealth.index=F)
+# charts.PerformanceSummary(theReturn(data.zoo), wealth.index=F)
 
-zoo.df = fortify(data.zoo, melt = TRUE)
 
-ggplot(aes(x = Index, y = Value), data = zoo.df) +
-  geom_line() + xlab("Index") + ylab("total") + 
-  facet_grid(Series~., scale="free_y")   
-plot.zoo(data.xts$roll)
-#plot.zoo(data$all)
-#chart.TimeSeries(data$roll )
-chart.CumReturns(data.zoo$roll, wealth.index=T)
+
+# theReturn <- function(new) {
+	# old <- lag(new)
+	# ret <- (new-old)/old
+	# return(ret)
+# }
+
+# data.zoo <- subset(data.zoo, select=all)
