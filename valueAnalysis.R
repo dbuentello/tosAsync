4c1a00c2696f3ebad965b1bb00c81fd688bb03f1
David Shanabrook <davidshanabrook@me.com>
2015-09-27 22:44:29 -0400

    cleanUp
    remove all unused code.  no its

--- valueAnalysis.R
+++ valueAnalysis.R
@@ -3,54 +3,25 @@ library("ggplot2")
 library("PerformanceAnalytics")
 library("xts")
 library(scales)
-valueFile <- "~/ShinyApps/tosAsync/out/value.csv"
-<<<<<<< Updated upstream
-#timeFormat <- "%Y-%m-%d %H:%M:%S"
-#data <- read.csv(valueFile)
-data.zoo <- read.zoo(valueFile, header=T, sep=",")
-#data.xts <- as.xts(data.zoo)
-#data.zoo <- subset(data.zoo, select=-c(mom,dad))
-
-#wealth or not
-theData <- data.zoo
-theData <- toWealth(data.zoo)
-#data.zoo$mine <- data.zoo$roll + data.zoo$ira + data.zoo$reg
-
-#http://www.inside-r.org/packages/cran/zoo/docs/autoplot.zoo
-zoo.df = fortify(theData, melt = TRUE)
-=======
-timeFormat <- "%Y-%m-%d %H:%M:%S"
-data <- read.csv(valueFile)
-data.zoo <- read.zoo(valueFile, header=T, sep=",")
-data.xts <- as.xts(data.zoo)
-data.zoo <- subset(data.zoo, select=-c(mom,dad))
-data.zoo$all <- rowSums(data.zoo)
-#data.zoo$mine <- data.zoo$roll + data.zoo$ira + data.zoo$reg
-
-#http://www.inside-r.org/packages/cran/zoo/docs/autoplot.zoo
-zoo.df = fortify(data.zoo, melt = TRUE)
->>>>>>> Stashed changes
-
-p <- ggplot(aes(x = Index, y = Value, group=Series,colour=Series), data = zoo.df) + geom_line() + xlab("Index") +  scale_y_continuous("Value $", labels=comma) 
-#+ scale_x_date()
-  p + facet_grid(Series~., scale="free_y", labeller="label_value")  
-  p  
-
-# chart.CumReturns(data.zoo$, wealth.index=F)
-# charts.PerformanceSummary(theReturn(data.zoo), wealth.index=F)
-
-
-
-# theReturn <- function(new) {
-	# old <- lag(new)
-	# ret <- (new-old)/old
-	# return(ret)
-# }
-# data.zoo <- subset(data.zoo, select=all)
-
 
 toWealth <- function(x) {
 	initial <- as.vector(x[1,])
 	coredata(x) <- sweep(x,MARGIN=2,initial,'/')
 	return(x)
 }
+
+valueFile <- "~/ShinyApps/tosAsync/out/value.csv"
+data.zoo <- read.zoo(valueFile, header=T, sep=",")
+data.zoo <- subset(data.zoo, select=-c(mom,dad))
+
+#wealth or not
+theData <- data.zoo
+theData <- toWealth(data.zoo)
+
+#http://www.inside-r.org/packages/cran/zoo/docs/autoplot.zoo
+zoo.df = fortify(data.zoo, melt = TRUE)
+
+p <- ggplot(aes(x = Index, y = Value, group=Series,colour=Series), data = zoo.df) + geom_line() + xlab("Index") +  scale_y_continuous("Value $", labels=comma) 
+#+ scale_x_date()
+  p + facet_grid(Series~., scale="free_y", labeller="label_value")  
+  p  
