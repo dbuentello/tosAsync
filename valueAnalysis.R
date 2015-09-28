93358eb5dd5f53c86e1f97f4716315354d4985d4
David Shanabrook <davidshanabrook@me.com>
2015-09-27 20:06:46 -0400

    addedWealth
    changed to using wealth analysis.  can still plot normally

--- valueAnalysis.R
+++ valueAnalysis.R
@@ -4,16 +4,18 @@ library("PerformanceAnalytics")
 library("xts")
 library(scales)
 valueFile <- "~/ShinyApps/tosAsync/out/value.csv"
-timeFormat <- "%Y-%m-%d %H:%M:%S"
-data <- read.csv(valueFile)
+#timeFormat <- "%Y-%m-%d %H:%M:%S"
+#data <- read.csv(valueFile)
 data.zoo <- read.zoo(valueFile, header=T, sep=",")
-data.xts <- as.xts(data.zoo)
-data.zoo <- subset(data.zoo, select=-c(mom,dad))
-data.zoo$all <- rowSums(data.zoo)
+#data.xts <- as.xts(data.zoo)
+#data.zoo <- subset(data.zoo, select=-c(mom,dad))
+#data.zoo$all <- rowSums(data.zoo)
+
+theData <- toWealth(data.zoo)
 #data.zoo$mine <- data.zoo$roll + data.zoo$ira + data.zoo$reg
 
 #http://www.inside-r.org/packages/cran/zoo/docs/autoplot.zoo
-zoo.df = fortify(data.zoo, melt = TRUE)
+zoo.df = fortify(theData, melt = TRUE)
 
 p <- ggplot(aes(x = Index, y = Value, group=Series,colour=Series), data = zoo.df) + geom_line() + xlab("Index") +  scale_y_continuous("Value $", labels=comma) 
 #+ scale_x_date()
@@ -30,5 +32,11 @@ p <- ggplot(aes(x = Index, y = Value, group=Series,colour=Series), data = zoo.df
 	# ret <- (new-old)/old
 	# return(ret)
 # }
-
 # data.zoo <- subset(data.zoo, select=all)
+
+
+toWealth <- function(x) {
+	initial <- as.vector(x[1,])
+	coredata(x) <- sweep(x,MARGIN=2,initial,'/')
+	return(x)
+}
