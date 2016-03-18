50e1da750798232f8cdd8598c53fabcbbac68cd8
David Shanabrook <davidshanabrook@me.com>
2016-03-17 23:33:31 -0400

    logs

--- asyncValue.R
+++ asyncValue.R
@@ -17,4 +17,4 @@ acctV <- function(accountid) {
 sessionid <<- TDALogin("TSLU", "1.0", myLoginName, myPassWord)
 
 value.df <- data.frame(time=Sys.time(),reg=acctV(dhsID),ira=acctV(iraID),roll=acctV(rollID),roth=acctV(rothID),mom=acctV(momID),dad=acctV(dadID))
-write.table(value.df, valueFile, append=T, sep=",",row.names=F, col.names=F)
+write.table(value.df, append=T, sep=",",row.names=F, col.names=F)
